9f195861268540432ce5acd618141d568412a99b
mlmhl <malinbupt@gmail.com>
2018-03-31 17:32:39 +0800

    add parallel mode for master

--- pkg/master/master.go
+++ pkg/master/master.go
@@ -2,33 +2,55 @@ package master
 
 import (
 	"fmt"
+	"time"
 
+	"github.com/mlmhl/mapreduce/pkg/rpc"
 	"github.com/mlmhl/mapreduce/pkg/types"
+	"github.com/mlmhl/mapreduce/pkg/util"
 
 	"github.com/golang/glog"
-	"github.com/mlmhl/mapreduce/pkg/util"
 )
 
-func New(job types.Job, mode types.Mode, inputFiles []string) (*Master, error) {
-	if err := validate(job, mode, inputFiles); err != nil {
+func New(config types.MasterConfiguration) (*Master, error) {
+	if err := validate(config); err != nil {
 		return nil, err
 	}
-	var scheduler scheduler
-	var taskManager taskManager
-	switch mode {
-	case types.Sequential:
-		taskManager = newSequentialTaskManager(job, inputFiles)
-		scheduler = newSequentialScheduler(job, taskManager, util.NewExecutorFactory())
+
+	m := &Master{
+		job:          config.Job,
+		taskManager:  newTaskManager(config.Job, config.InputFiles),
+		finishedChan: make(chan struct{}),
 	}
-	return &Master{scheduler: scheduler, taskManager: taskManager, finishedChan: make(chan struct{})}, nil
+
+	var scheduler scheduler
+	var workerManager workerManager
+	switch config.Mode {
+	case types.Sequential:
+		scheduler = newSequentialScheduler(config.Job, m.taskManager, util.NewExecutorFactory())
+	case types.Parallel:
+		rpcServer, err := newServer(m, config.Address)
+		if err != nil {
+			return nil, err
+		}
+		m.rpcServer = rpcServer
+		workerManager = newWorkerManager(config.WorkerTimeout, config.CheckInterval)
+		scheduler = newParallelScheduler(config.Job, m.taskManager, workerManager)
+	default:
+		return nil, fmt.Errorf("unknown mode: %s", config.Mode)
+	}
+
+	m.scheduler = scheduler
+	m.workerManager = workerManager
+
+	return m, nil
 }
 
-func validate(job types.Job, mode types.Mode, inputFiles []string) error {
-	if job.MapNum != len(inputFiles) {
-		return fmt.Errorf("mismatch map number(%d) and input file number(%d)", job.MapNum, len(inputFiles))
+func validate(config types.MasterConfiguration) error {
+	if config.Job.MapNum != len(config.InputFiles) {
+		return fmt.Errorf("mismatch map number(%d) and input file number(%d)", config.Job.MapNum, len(config.InputFiles))
 	}
-	if !types.ValidaMode(mode) {
-		return fmt.Errorf("unsupported mode: %s", mode)
+	if !types.ValidaMode(config.Mode) {
+		return fmt.Errorf("unsupported mode: %s", config.Mode)
 	}
 	return nil
 }
@@ -41,12 +63,28 @@ type Master struct {
 	finalStatus types.JobStatus
 
 	finishedChan chan struct{}
+
+	// Special for parallel mode.
+	rpcServer     *Server
+	workerManager workerManager
 }
 
 func (m *Master) Run() types.JobStatus {
-	defer close(m.finishedChan)
 	defer glog.Infof("Job %s completed", m.job.Name)
 	glog.Infof("Job %s started", m.job.Name)
+
+	defer close(m.finishedChan)
+
+	if m.rpcServer != nil {
+		m.rpcServer.run()
+		defer m.rpcServer.stop()
+	}
+
+	if m.workerManager != nil {
+		m.workerManager.Run()
+		defer m.workerManager.Stop()
+	}
+
 	output, err := m.scheduler.Run()
 	status := m.taskManager.Status()
 	if err == nil {
@@ -60,15 +98,14 @@ func (m *Master) Run() types.JobStatus {
 }
 
 func (m *Master) Go() {
-	go func() {
-		m.Run()
-	}()
+	go m.Run()
 }
 
 func (m *Master) Status() types.JobStatus {
 	select {
 	case <-m.finishedChan:
 		return m.finalStatus
+	default:
 	}
 	status := m.taskManager.Status()
 	status.Message = "Running"
@@ -85,4 +122,29 @@ func (m *Master) Finished() bool {
 
 func (m *Master) Wait() {
 	<-m.finishedChan
+}
+
+func (m *Master) addWorker(name string, balance int, address rpc.Address) error {
+	w, err := newWorker(name, address)
+	if err != nil {
+		return err
+	}
+	m.workerManager.Add(w, balance)
+	return nil
+}
+
+func newWorker(name string, address rpc.Address) (worker, error) {
+	client, err := rpc.NewClient(address)
+	if err != nil {
+		return worker{}, err
+	}
+	return worker{
+		name:       name,
+		lastActive: time.Now(),
+		client:     client,
+	}, nil
+}
+
+func (m *Master) workerHeartBeat(name string) {
+	m.workerManager.HeartBeat(name)
 }
